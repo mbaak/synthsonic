4f1aaa7b63e1c6cf5bde690b380266b2bfdccba0
sbrugman-ing <66009968+sbrugman-ing@users.noreply.github.com>
2020-09-22 11:48:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='synthsonic',
-    packages=['synthsonic'],
+    packages=find_packages(),
     version='0.1.0',
     description='Super realistic data modelling and synthesis',
     author='ING WBAA',
