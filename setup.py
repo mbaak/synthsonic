906463e8f005a5057663d954e3f86cabfebf2133
Ralph Urlus <rurlus@Ralphs-MacBook-Pro.local>
2021-06-04 16:28:10 +0200

    Install local third-party forks

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
@@ -14,12 +15,21 @@ EXTRA_REQUIREMENTS = {
     'test': TEST_REQUIREMENTS,
 }
 
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+
+REQUIREMENTS.append(
+    'pgmpy @ file://localhost{}/third_party/pgmpy_dev'.format(PKG_DIR)
+)
+REQUIREMENTS.append(
+    'sdgym @ file://localhost{}/third_party/sdgym-0.2.2.fork'.format(PKG_DIR)
+)
+
 setup(
     name='synthsonic',
     packages=find_packages(),
     version='0.1.0',
     description='Super realistic data modelling and synthesis',
-    author='ING WBAA',
+    author='anon_for_review',
     license='MIT',
     install_requires=REQUIREMENTS,
     extras_require=EXTRA_REQUIREMENTS,
